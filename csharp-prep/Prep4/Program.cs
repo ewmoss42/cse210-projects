50d64d7ff9570769a07bd70bc80ffd2802f90bdd
ewmoss42 <ewmoss@byui.edu>
2024-02-29 11:50:28 -0700

    finished prep 4 first test.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int user_number = -1;
+        while (user_number !=0)
+        {
+            Console.Write("Enter a number (0 to quit): ");
+
+            string user_response = Console.ReadLine();
+            user_number = int.Parse(user_response);
+
+            if (user_number != 0)
+            {
+                numbers.Add(user_number);
+            }
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sm is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
     }
 }
