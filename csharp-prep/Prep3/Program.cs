48d2c96d79f4f52001db3031f1c12838c53d7288
ewmoss42 <ewmoss@byui.edu>
2024-02-29 11:32:19 -0700

    Finished the guessing game structure

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator= new Random();
+        int magic_number = randomGenerator.Next(1, 101);
+        
+        int guess_number = -1;
+
+        while (guess_number != magic_number)
+        {
+            Console.Write("What is your guess? ");
+            guess_number = int.Parse(Console.ReadLine());
+            
+            if (magic_number > guess_number)
+            {
+                Console.WriteLine("Higer");
+            }
+            else if (magic_number < guess_number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else 
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
