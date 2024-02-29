fb3498030fe95fe5fad6f98cd4654117294cebbf
ewmoss42 <ewmoss@byui.edu>
2024-02-29 09:51:56 -0700

    fixed last name console readline

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -8,7 +8,7 @@ class Program
         string first_name = Console.ReadLine();
 
         Console.Write("What is your last name? ");
-        string last_name
+        string last_name = Console.ReadLine();
 
         Console.WriteLine($"Your Name is {last_name}, {first_name} {last_name}.");
     }
