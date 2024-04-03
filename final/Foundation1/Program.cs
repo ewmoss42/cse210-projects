da2ef2d6d51e7e6634d2c6c16a8561b8beb8d27c
ewmoss42 <ewmoss@byui.edu>
2024-04-03 16:03:43 -0600

    more details added

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -6,37 +6,37 @@ class Program
     {
         List<Video> videos = new List<Video>();
 
-        Video video1 = new Video("Title", "Author", "Length");
-        video1.comment("Name", "comment");
-        video1.comment("Name", "comment");
-        video1.comment("Name", "comment");
-        video1.comment("Name", "comment");
+        Video video1 = new Video("Funny Moments", "Theodore", "203");
+        video1.comment("Noah", "A great video");
+        video1.comment("Sophia", "Enjoy your content");
+        video1.comment("Henry", "First!");
+        video1.comment("Ava", "Second?");
         videos.Add(video1);
         
 
-        Video video2 = new Video("Title", "Author", "Length");
-        video2.comment("Name", "comment");
-        video2.comment("Name", "comment");
-        video2.comment("Name", "comment");
-        video2.comment("Name", "comment");
+        Video video2 = new Video("A study of Egypt", "Benjamin", "803");
+        video2.comment("Luna", "Very informative");
+        video2.comment("Lucas", "Need to work on Pronouncing names");
+        video2.comment("Oliver", "Not gonna lie, I expected more");
+        video2.comment("Liam", "Told me things I didn't know");
         videos.Add(video2);
       
         
 
-        Video video3 = new Video("Title", "Author", "Length");
+        Video video3 = new Video("Zero to Hero", "Elijah", "214");
        
-        video3.comment("Name", "comment");
-        video3.comment("Name", "comment");
-        video3.comment("Name", "commento");
-        video3.comment("Name", "comment");
+        video3.comment("Emma", "Very Inpsiring");
+        video3.comment("William", "I totally relate to this ");
+        video3.comment("Jack", "Love this trope ");
+        video3.comment("Hazel", "Name of song?");
         videos.Add(video3);
         
 
-        Video video4 = new Video("Title", "Author", "Length");
-        video4.comment("Name", "comment");
-        video4.comment("Name", "Wcomment");
-        video4.comment("Name", "comment");
-        video4.comment("Name", "comment");
+        Video video4 = new Video("9 signs You're a Expert", "Charlotte", "514");
+        video4.comment("Willow", "Guess there are 9 signs I am not an expert");
+        video4.comment("Kai", "First");
+        video4.comment("Samual", "love the satire");
+        video4.comment("Abigail", "love your content");
         videos.Add(video4);
       
 
