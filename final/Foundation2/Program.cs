9ed33fb2597608aed291f7b0b194a5bc09bb1c11
ewmoss42 <ewmoss@byui.edu>
2024-04-04 16:52:24 -0600

    finished added files to foundation 3

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -6,13 +6,13 @@ class Program
     static void Main(string[] args)
     {
         
-        Address address1 = new Address("Angamos St 125", "San Borja", "Lima", "Peru");
-        Customer customer1 = new Customer("Valeria Alcala Guillen", address1);
+        Address address1 = new Address("3 Petworth Rd", "Dunestone", "TQ13 1ZD", "UK");
+        Customer customer1 = new Customer("Willaim Daniel Smith ", address1);
         List<Product> productsList1 = new List<Product>();
 
-        Product p1001 = new Product("Graphing Calculator", "GC200", 65.99, 1);
+        Product p1001 = new Product("Graphing Calculator", "GC200", 65.99, 6);
         Product p1002 = new Product("Notebook", "NB50", 3.50, 5);
-        Product p1003 = new Product("Mechanical Pencil", "MP05", 1.99, 10);
+        Product p1003 = new Product("Mechanical Pencil", "MP05", 1.99, 30);
 
         productsList1.Add(p1001);
         productsList1.Add(p1002);
@@ -25,13 +25,13 @@ class Program
         Console.WriteLine();
 
         
-        Address address2 = new Address("352 St", "Good Year", "AZ 85001", "USA");
-        Customer customer2 = new Customer("Blake A. Larsen", address2);
+        Address address2 = new Address("1471 Tamarack Pl", "Woodbridge", "VA 22191", "USA");
+        Customer customer2 = new Customer("Eric B. Harrison", address2);
         List<Product> productsList2 = new List<Product>();
 
-        Product p2001 = new Product("Highlighter Set", "HLSET", 4.99, 3);
-        Product p2002 = new Product("Index Cards", "IC100", 1.75, 2);
-        Product p2003 = new Product("Scientific Calculator", "SCC1000", 12.50, 1);
+        Product p2001 = new Product("Highlighter Set", "HLSET", 4.99, 4);
+        Product p2002 = new Product("Index Cards", "IC100", 1.75, 3);
+        Product p2003 = new Product("Scientific Calculator", "SCC1000", 12.50, 5);
 
         productsList2.Add(p2001);
         productsList2.Add(p2002);
@@ -44,8 +44,8 @@ class Program
         Console.WriteLine();
 
         
-        Address address3 = new Address("2010 W 500 S", "Salt Lake", "UT 84104", "USA");
-        Customer customer3 = new Customer("Dario G. Mostacero", address3);
+        Address address3 = new Address("136 Bear Dr", "Sigel", "PA 15860", "USA");
+        Customer customer3 = new Customer("Luigi W Montell", address3);
         List<Product> productsList3 = new List<Product>();
 
         Product p3001 = new Product("Pencil Case", "PC100", 8.99, 2);
