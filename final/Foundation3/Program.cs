67af8c9d111b1769b2b410775025b4e0aa335739
ewmoss42 <ewmoss@byui.edu>
2024-04-08 15:02:47 -0600

    finished foundation 3

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -5,15 +5,15 @@ class Program
     static void Main(string[] args)
     {
        
-        LectureEvent LE = new LectureEvent("Lecture", "Meet Elder Holland", "Personal Fireside with Elder Holland", "4th of April", "7pm", "Blackfoot South Stake Center Building","Elder Holland", "300");
+        LectureEvent LE = new LectureEvent("Lecture", "Pyton Basics", "Introduction to Python programming", "4th of April", "7pm", "Blackfoot South Stake Center Building","William Thompson", "200");
         Console.WriteLine(LE.GetStandard());
         Console.WriteLine(LE.GetFullLecture());
         Console.WriteLine(LE.GetShort());
-        ReceptionEvent RE = new ReceptionEvent("Reception", "Carrie & Devan's Wedding", "A small reception to celebrate our wedding", "April 8th", "8pm", "204 Johnson rd, Blackfoot ID 83221", "ubenhaded@gmail.com");
+        ReceptionEvent RE = new ReceptionEvent("Reception", "Samuel and Jennifer's Wedding", "A small reception to celebrate our wedding", "March 14th", "6pm", "204 Johnson rd, Blackfoot ID 83221", "ubenhaded@gmail.com");
         Console.WriteLine(RE.GetStandard());
         Console.WriteLine(RE.GetFullReception());
         Console.WriteLine(RE.GetShort());
-        OutdoorEvent OE = new OutdoorEvent("Outdoor", "Young Mens Campout", "Going to the Thompson's Land for an overnighter, we will carpool from the church", "Saturday the 1st", "7am", "Blackfoot South Stake Building", "Cloudy");
+        OutdoorEvent OE = new OutdoorEvent("Outdoor", "Young Mens Campout", "Enjoy Camping in the park with food and games", "Saturday the 1st", "9am", "Blackfoot South Stake Building", "Cloudy");
         Console.WriteLine(OE.GetStandard());
         Console.WriteLine(OE.GetFullOutdoor());
         Console.WriteLine(OE.GetShort());
